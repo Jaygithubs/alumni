b2149f76b15419c853979f8cec892e0682f28513
Jay sharma <jaysharma895@gmail.com>
2026-03-09 21:04:01 +0530

    put logo in the website

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
           href="/"
           className="text-xl font-bold text-[rgb(18_39_87/98%)]"
         >
-          Alumni Network
+          <img src="/Logo ashoka.png" alt="logo" className="h-20 w-auto" />
         </Link>
 
         {/* Desktop Menu */}
